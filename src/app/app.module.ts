2ad4ad908783206f8370e0d1905653fb5cb11df8
colantuomo <pcolantuomo@gmail.com>
2019-04-22 23:06:53 -0300

    inserting card to UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { MenuModule } from './shared/menu/menu.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     MenuModule,
     HttpClientModule,
     AppRoutingModule
--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { TasksService } from 'src/app/services/tasks.service';
+import { trigger, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
+  animations: [
+    trigger('divState', [
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
+          style({ opacity: 1, transform: 'translateX(15px)', offset: 0.3 }),
+          // style({ opacity: 1, transform: 'translateX(0)', offset: 1.0 })
+        ]))
+      ]),
+      // transition('* => void', [
+      //   animate(300, keyframes([
+      //     style({ opacity: 1, transform: 'translateX(0)', offset: 0 }),
+      //     style({ opacity: 1, transform: 'translateX(-15px)', offset: 0.7 }),
+      //     style({ opacity: 0, transform: 'translateX(100%)', offset: 1.0 })
+      //   ]))
+      // ])
+    ])
+  ]
 })
 export class TasksComponent implements OnInit, AfterViewInit {
 
