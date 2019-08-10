628ea78dd6b6d48c244dc08b942daa0519d8a84e
Colantuomo <pcolantuomo@gmail.com>
2019-08-09 22:26:17 -0300

    Changing UI, creating logic to components and mocking data for testing

--- src/app/shared/components/card-list/card-list.component.ts
+++ src/app/shared/components/card-list/card-list.component.ts
@@ -7,9 +7,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardListComponent implements OnInit {
 
+  items = [];
   constructor() { }
 
   ngOnInit() {
+    this.items = [
+      {
+        month: 'January', year: 2019, cards: [
+          {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+          }
+        ]
+      },
+      {
+        month: 'February', year: 2019, cards: [
+          {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut autem",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+          },
+          {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+            "completed": true
+          },
+          {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+          },
+        ]
+      },
+    ]
   }
 
 }
